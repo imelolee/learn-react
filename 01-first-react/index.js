9d0cb71a75ce006fc5efe7cf93c6485b5f686892
メロ <meloligen@gmail.com>
2023-02-05 22:07:46 +0800

    first react

---
+++ 01-first-react/index.js
@@ -0,0 +1,7 @@
+ReactDOM.render(<h2>Hello, React.</h2>, document.getElementById("root"))
+
+ReactDOM.render(
+    <ul><li>Thing 1</li><li>Thing 2</li></ul>, 
+    document.getElementById("list")
+)
+
