0238c55a8ab5ea13750b143e16a3996e6dcf77b3
imelolee <meloligen@gmail.com>
2023-02-08 22:05:28 +0800

    react components

--- 02-react-page/src/index.js
+++ 02-react-page/src/index.js
@@ -1,15 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+Challenge: Starting from scratch, build and render the 
+HTML for our section project. Check the Google slide for 
+what you're trying to build.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+We'll be adding styling to it later.
+
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+const page = ReactDOM.createRoot(document.getElementById("root"));
+
+function MyHeader() {
+  return (
+    <header>
+      <nav className="nav">
+        <img src="./react-logo.png" className="nav-logo" />
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>Docs</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function MyContext() {
+  return (
+    <div>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+        <li>...</li>
+      </ul>
+    </div>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <MyHeader />
+      <MyContext />
+    </div>
+  );
+}
+
+page.render(<Page />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
